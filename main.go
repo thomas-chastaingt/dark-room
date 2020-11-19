30b8b2b04755f4d4134bba1128d8f928f0d41c83
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2020-11-19 13:55:25 +0100

    divide and conquer

--- main.go
+++ main.go
@@ -2,98 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
-	"path"
-	"strings"
 
 	"github.com/gorilla/websocket"
-	gubrak "github.com/novalagung/gubrak/v2"
 )
-
-type M map[string]interface{}
-
-const MESSAGE_NEW_USER = "New User"
-const MESSAGE_CHAT = "Chat"
-const MESSAGE_LEAVE = "Leave"
-
-var connections = make([]*WebSocketConnection, 0)
-
-type SocketPayload struct {
-	Message string
-}
-
-type SocketResponse struct {
-	From    string
-	Type    string
-	Message string
-}
-
-type WebSocketConnection struct {
-	*websocket.Conn
-	Username string
-}
-
-func HandleRoom(w http.ResponseWriter, r *http.Request) {
-	fp := path.Join("web/templates", "room.html")
-	tmpl, err := template.ParseFiles(fp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if err := tmpl.Execute(w, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func handleIO(currentConn *WebSocketConnection, connections []*WebSocketConnection) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("ERROR", fmt.Sprintf("%v", r))
-		}
-	}()
-
-	broadcastMessage(currentConn, MESSAGE_NEW_USER, "")
-
-	for {
-		payload := SocketPayload{}
-		err := currentConn.ReadJSON(&payload)
-		if err != nil {
-			if strings.Contains(err.Error(), "websocket: close") {
-				broadcastMessage(currentConn, MESSAGE_LEAVE, "")
-				ejectConnection(currentConn)
-				return
-			}
-
-			log.Println("ERROR", err.Error())
-			continue
-		}
-
-		broadcastMessage(currentConn, MESSAGE_CHAT, payload.Message)
-	}
-}
-
-func ejectConnection(currentConn *WebSocketConnection) {
-	filtered := gubrak.From(connections).Reject(func(each *WebSocketConnection) bool {
-		return each == currentConn
-	}).Result()
-	connections = filtered.([]*WebSocketConnection)
-}
-
-func broadcastMessage(currentConn *WebSocketConnection, kind, message string) {
-	for _, eachConn := range connections {
-		if eachConn == currentConn {
-			continue
-		}
-
-		eachConn.WriteJSON(SocketResponse{
-			From:    currentConn.Username,
-			Type:    kind,
-			Message: message,
-		})
-	}
-}
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
